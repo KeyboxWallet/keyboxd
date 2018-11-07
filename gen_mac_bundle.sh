a1ed6cdb9545ac5a5e8c966fce06a49bc58beded
Zhang Zengbo <zengbo.zhang@gmail.com>
2018-11-07 15:01:24 +0800

    gen_mac_bundle.sh: fix an issue

--- gen_mac_bundle.sh
+++ gen_mac_bundle.sh
@@ -3,7 +3,7 @@ cmake -DCMAKE_BUILD_TYPE=Release . &&
 make -j3 &&
 macdeployqt keyboxd.app &&
 cd qt &&
-cmake -DCMAKE_BUILD_TYPE=Relase . &&
+cmake -DCMAKE_BUILD_TYPE=Release . &&
 make -j3 &&
 macdeployqt keyboxd_ui.app &&
 cp ../keyboxd.app/Contents/MacOS/keyboxd keyboxd_ui.app/Contents/MacOS &&
