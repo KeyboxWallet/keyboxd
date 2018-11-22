c1001f5de4e0376d404edfac54878b74770bd841
Zhang Zengbo <zengbo.zhang@gmail.com>
2018-11-22 18:07:46 +0800

    install to /usr/local for mac

--- gen_mac_bundle.sh
+++ gen_mac_bundle.sh
@@ -1,10 +1,14 @@
 git clean . -xdf &&
 cmake -DCMAKE_BUILD_TYPE=Release . &&
 make -j3 &&
-macdeployqt keyboxd.app &&
-codesign --deep --force --verbose --sign 'Developer ID App' keyboxd.app/ &&
-mkdir -p osx-root/Applications &&
-mv keyboxd.app osx-root/Applications &&
+mkdir keyboxd.app/Contents/libs &&
+local_libs=`otool -L keyboxd.app/Contents/MacOS/keyboxd | grep /usr/local | awk '{print $1}'` &&
+for lib in $local_libs ; do 
+   cp $lib keyboxd.app/Contents/libs
+   install_name_tool -change $lib @executable_path/../libs/`basename $lib` keyboxd.app/Contents/MacOS/keyboxd
+done &&
+mkdir -p osx-root/usr/local &&
+mv keyboxd.app osx-root/usr/local/keyboxd &&
 mkdir -p osx-root/Library/LaunchAgents &&
 cp mac/org.keyboxwallet.daemon.plist osx-root/Library/LaunchAgents &&
 pkgbuild  --root osx-root/ --identifier org.keyboxwallet.daemon --scripts mac/scripts --component-plist mac/components.plist --version 0.3.0 out.pkg &&
